59b93071a3981efb6604bc454748179cfedfe92a
hellotunamayo <yoo@minyoungyoo.com>
2023-09-06 21:51:37 +0900

    initial commit

---
+++ UIKitPlayground/CombineRealtimeUpdateViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  UIKitPlayground
+//
+//  Created by Minyoung Yoo on 2023/08/29.
+//
+
+import UIKit
+import Combine
+
+class RealtimeUpdatingViewControllerViewModel: ObservableObject {
+    @Published var name: String = "John Doe"
+    @Published var secondName: String = "Hong Gildong"
+    
+    func changeFirstName(name: String){
+        self.name = name
+    }
+    
+    func changeSecondName(name: String){
+        self.secondName = name
+    }
+}
+
+class CombineRealtimeUpdateViewController: UIViewController {
+    let vm: RealtimeUpdatingViewControllerViewModel = RealtimeUpdatingViewControllerViewModel()
+    let uiLabel: UILabel = UILabel()
+    let secondUILabel: UILabel = UILabel()
+    private var cancellables: Set<AnyCancellable> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        view.backgroundColor = UIColor.systemBackground
+        
+        vm.$name.sink { [weak self] value in
+            self?.uiLabel.text = value
+        }.store(in: &cancellables)
+        
+        vm.$secondName.sink { [weak self] value in
+            self?.secondUILabel.text = value
+        }.store(in: &cancellables)
+        
+        let button: UIButton = UIButton()
+        
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        button.setTitle("Tap to change name", for: .normal)
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        
+        view.addSubview(button)
+        view.addSubview(uiLabel)
+        view.addSubview(secondUILabel)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        uiLabel.translatesAutoresizingMaskIntoConstraints = false
+        secondUILabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: secondUILabel.bottomAnchor, constant: 10),
+            button.widthAnchor.constraint(equalToConstant: 300),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            uiLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            uiLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            secondUILabel.centerXAnchor.constraint(equalTo: uiLabel.centerXAnchor),
+            secondUILabel.topAnchor.constraint(equalTo: uiLabel.bottomAnchor, constant: 10)
+        ])
+    }
+
+    @objc func tapAction() -> Void {
+        vm.changeFirstName(name: "Mary Jane")
+        vm.changeSecondName(name: "Go Gildong")
+    }
+}
+
---
+++ UIKitPlayground/DidsetRealtimeUpdateViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  UIKitPlayground
+//
+//  Created by Minyoung Yoo on 2023/08/29.
+//
+
+//Didset으로 뷰 업데이트하기 예제
+
+import UIKit
+import Combine
+
+class ViewModel {
+    var number: Int = 0
+    func increase() {
+        self.number += 1
+    }
+}
+
+class DidsetRealtimeUpdateViewController: UIViewController {
+    let vm: ViewModel = ViewModel()
+    let uiLabel: UILabel = UILabel()
+    var counting: Int = 0 {
+        didSet {
+            uiLabel.text = "\(counting)"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        uiLabel.text = "\(counting)"
+
+        let button: UIButton = UIButton()
+        
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        button.setTitle("Tap", for: .normal)
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        
+        view.addSubview(button)
+        view.addSubview(uiLabel)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        uiLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: uiLabel.bottomAnchor, constant: 50),
+            button.widthAnchor.constraint(equalToConstant: 300),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            uiLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            uiLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    @objc func tapAction() -> Void {
+        vm.increase()
+        counting = vm.number
+    }
+}
+
